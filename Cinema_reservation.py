34c317b6c669be04f51c6a8819662524eb116d2e
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-12-09 21:38:48 +0100

    Update Cinema_reservation.py

--- Cinema_reservation.py
+++ Cinema_reservation.py
@@ -1,63 +1,79 @@
-# Indexierung der Sitzplätze:  Sitzplatznummern bei 1 beginnen zu lassen und die Anpassung in der Methode set_seat_status
 # Fehlerprüfung in set_seat_status: Überprüfung dass übergebenen Sitznummern gültig sind (z.B. dass sie nicht größer als total_seats sind).
-# Vermeidung von Off-by-One-Fehlern: Die Methode set_seat_status fügt 1 zu jedem Index hinzu (i+1), was zu einem Off-by-One-Fehler führen kann.
 # Direkte Zuweisung der Sitzplätze im Konstruktor: Anstelle einer Schleife im Konstruktor von Room könntest du ein Dictionary Comprehension verwenden, um den Code kompakter und Python-typischer zu gestalten.
-# Sitzplatzkategorien und verschiedene Preise für Sitzplätze. Reservierung ergibt Gesamtsumme. 
 # Uhrzeiten für Filme
-# Kundenkonto für Karten und Zahlvorgänge + validierung bspw. Kreditkarte
-# Führe eine Sitzplatzklasse ein, in der es normale und Premiumsitze gibt, die verschieden Kosten.
-# Implementiere in der Kino-Klasse eine Methode __str__, die eine formatierte Darstellung des Kinos zurückgibt.
-
+# Kundenkonto für Karten und Zahlvorgänge + validierung bspw. Kreditkarte. Einkaufswagen mit Einzelposten und Gesamtsumme
 
 class Cinema:
     def __init__(self, name, location):
         self.name = name
         self.location = location
+        self.rooms = {}
+      
+    def add_room(self, room):
+        self.rooms[room.room_number] = room
     
     def __str__(self):
         return f"Cinema: {self.name}\nLocation: {self.location}"
+    
+    def view_rooms(self):
+        return "\n".join(str(room)for room in self.rooms.values())  
 
 class Room:
-    def __init__(self,room_number, total_seats, cinema):
+    def __init__(self,room_number, cinema, normal_seats, premium_seats):
         self.room_number = room_number
         self.cinema = cinema
-        self.total_seats = total_seats
-        self.seats = {}
-        for i in range(self.total_seats):
-            self.seats[i] = "Free"
-       
-    def set_seat_status(self, numbers):
-        for i in numbers:
-            self.seats[i+1] = "Reserved" 
+        self.seats = {i: Seat_normal(i, self) for i in range(normal_seats+1)}
+        self.seats.update({i: Seat_premium(i, self) for i in range(normal_seats +1, normal_seats +1 + premium_seats)})
+
+    def list_seats(self):
+        return "\n".join(str(seat) for seat in self.seats.values())
+ 
+    def __str__(self):
+        return f"Room number {self.room_number}"
 
 class Seat:
-    def __init__(self, number, category, room):
-        self.number = number
-        self.category = category
+    def __init__(self, seat, room):
+        self.seat_number = seat
         self.room = room
         self.status = "Free"
+    
+    def __str__(self):
+        return f"{self.category} Seat number {self.seat_number:02d}: {self.status}"
 
+class Seat_normal(Seat):
+    def __init__(self, seat, room):
+        super().__init__(seat, room)
+        self.room = room
+        self.category = "Normal"
+        self.price = 14.50
+
+class Seat_premium(Seat):
+    def __init__(self, seat, room):
+        super().__init__(seat, room)
+        self.room = room
+        self.category = "Premium"
+        self.price = 16.99
+
+class Movie:
+    def __init__(self, name):
+        self.name = name
 
 cineplex_muc = Cinema("Cineplex", "Munich")
-cineplex_muc_room1 = Room(1,20, cineplex_muc)
+cineplex_muc_room1 = Room(1, cineplex_muc, normal_seats= 35, premium_seats = 15)
+cineplex_muc_room2 = Room(2, cineplex_muc, normal_seats= 40, premium_seats = 10)
+palast_ber = Cinema("Filmpalast", "Berlin")
+palast_ber_room1 = Room(1, palast_ber, normal_seats = 25, premium_seats = 15)
+palast_ber_room2 = Room(2, palast_ber, normal_seats = 50, premium_seats = 20)                          
 
-seat_1_normal_room1_cineplex_muc = Seat(1, "normal", cineplex_muc_room1)
-seat_2_normal_room1_cineplex_muc = Seat(2, "normal", cineplex_muc_room1)
-seat_3_normal_room1_cineplex_muc = Seat(3, "normal", cineplex_muc_room1)
-seat_4_normal_room1_cineplex_muc = Seat(4, "normal", cineplex_muc_room1)
-seat_5_normal_room1_cineplex_muc = Seat(5, "normal", cineplex_muc_room1)
-seat_6_normal_room1_cineplex_muc = Seat(6, "normal", cineplex_muc_room1)
-seat_7_normal_room1_cineplex_muc = Seat(7, "normal", cineplex_muc_room1)
-seat_8_normal_room1_cineplex_muc = Seat(8, "normal", cineplex_muc_room1)
-seat_9_normal_room1_cineplex_muc = Seat(9, "normal", cineplex_muc_room1)
-seat_10_normal_room1_cineplex_muc = Seat(10, "normal", cineplex_muc_room1)
-seat_11_premium_room1_cineplex_muc = Seat(11, "premium", cineplex_muc_room1)
-seat_12_premium_room1_cineplex_muc = Seat(12, "premium", cineplex_muc_room1)
-seat_13_premium_room1_cineplex_muc = Seat(13, "premium", cineplex_muc_room1)
-seat_14_premium_room1_cineplex_muc = Seat(14, "premium", cineplex_muc_room1)
-seat_15_premium_room1_cineplex_muc = Seat(15, "premium", cineplex_muc_room1)
+action_movie = Movie("The Punishifinator 3D")
+romance_movie = Movie("The love is love")
+comedy_movie = Movie("Funny laughing 3")
 
+cineplex_muc.add_room(cineplex_muc_room1)
+cineplex_muc.add_room(cineplex_muc_room2)
 
+print(cineplex_muc_room1.list_seats())
+    
 
 # reserve = 12, 14, 15
 # cineplex_muc_room1.set_seat_status(reserve)
